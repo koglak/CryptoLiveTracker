be861ad79add04ffb250f8d9946cc6c542723dca
kardelenoglak <koglakci@ford.com.tr>
2024-08-13 18:17:06 +0300

    Pagination added

--- src/components/CryptoTable.tsx
+++ src/components/CryptoTable.tsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { CryptoData } from '../interfaces/CryptoData';
 import PriceText from './PriceText';
-import '../styles/table.css'
 import ChangeIndicator from './ChangeIndicator';
 import CryptoDetails from './CryptoDetails';
+import ResponsivePagination from 'react-responsive-pagination';
+import '../styles/pagination.css';
+import '../styles/table.css';
 
 const CryptoTable: React.FC = () => {
   const [cryptoData, setCryptoData] = useState<CryptoData[]>([]);
+  const [CurrentPage, setCurrentPage] = useState(1);
+  const [TotalPage, setTotalPage] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,19 +22,21 @@ const CryptoTable: React.FC = () => {
           price: parseFloat(item.lastPrice).toFixed(2),
           marketValue: (parseFloat(item.lastPrice) * parseFloat(item.volume)).toFixed(2),
           change: item.priceChangePercent,
-        }))
-        console.log(response.data)
-        setCryptoData(formattedData);
+        }));
+
+        setCryptoData(formattedData.slice((CurrentPage - 1) * 10, CurrentPage * 10));
+        setTotalPage(Math.ceil(formattedData.length / 10));
       } catch (error) {
         console.error('Error fetching data', error);
       }
     };
 
     fetchData();
+
     const interval = setInterval(fetchData, 5000); // Update every 5 seconds
 
     return () => clearInterval(interval); // Cleanup on component unmount
-  }, []);
+  }, [CurrentPage]); // Add CurrentPage as a dependency
 
   return (
     <div className='table-box'>
@@ -47,15 +53,24 @@ const CryptoTable: React.FC = () => {
         <tbody>
           {cryptoData.map((crypto) => (
             <tr key={crypto.symbol} className='table-row'>
-              <td><CryptoDetails CryptoName={crypto.symbol}/></td>
+              <td><CryptoDetails CryptoName={crypto.symbol} /></td>
               <td><PriceText Price={crypto.price} Unit={"USDT"} /></td>
               <td><PriceText Price={crypto.marketValue} Unit={"USDT"} /></td>
-              <td><ChangeIndicator Value={crypto.change}/></td>
+              <td><ChangeIndicator Value={crypto.change} /></td>
               <td></td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <ResponsivePagination
+        current={CurrentPage}
+        total={TotalPage}
+        onPageChange={setCurrentPage}
+        className="pagination"
+
+      />
+
     </div>
   );
 };
